0e78b747b0fb9050f4779be4122be0862c2297b3
Robert Fauver <robertfauver@gmail.com>
2014-07-03 12:36:16 -0400

    yeoman init, working on kitchen sink

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngResponsiveFormApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngResponsiveFormApp
+ */
+angular.module('ngResponsiveFormApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
